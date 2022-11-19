879cf53710271bd19bcf4b89e3e0e549bf689ace
elissandroa <elissandroa@hotmail.com>
2022-11-18 21:01:09 -0300

    H2 database

---
+++ backend/src/main/java/com/elissandro/dscatalog/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.elissandro.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.elissandro.dscatalog.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
